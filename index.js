27c958780d14ae85fbde23a3c745bc5bd5fe8486
root <iqbalforall@gmail.com>
2022-05-16 15:02:53 +0000

    add counter backend, get ..

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 const port = 3010
 
-
+var counter = 0;
 app.set('view engine', 'ejs');
 
 app.use('/static', express.static('/static'));
@@ -15,6 +15,20 @@ app.get('/users/:name', (req, res) => {
   // res.send('Hello name:' + name)
   res.render("user",{name:name})
 })
+
+app.put('/users/:name/counters/incr', (req, res) => {
+  var name = req.params.name || 'ikb'
+  // res.send('Hello name:' + name)
+  counter++;
+  res.json({counter: counter})
+})
+
+app.get('/users/:name/counters', (req, res) => {
+  var name = req.params.name || 'ikb'
+  // res.send('Hello name:' + name)
+  res.json({counter: counter});
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
